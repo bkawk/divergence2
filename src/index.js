4ca01decca62178f55edd3ef6604f744c011ed85
cipto <oci_beken@yahoo.co.id>
2018-05-27 09:57:45 +0700

    Upgrade to promise and refactor

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // @ts-check
-"use strict";
-const db = require("./db");
+'use strict';
+const db = require('./db');
 const nrc = require('node-run-cmd');
 const util = require('util');
-const getBitfinexData = require("./getBitfinexData");
-const tmp = require("tmp-promise");
-const chalk = require("chalk");
+const getBitfinexData = require('./getBitfinexData');
+const tmp = require('tmp-promise');
+const chalk = require('chalk');
 const connected = chalk.bold.cyan;
 const error = chalk.bold.yellow;
 
@@ -17,15 +17,30 @@ db()
   })
   .catch(() => {
     console.log(error('Mongo Connection Error'));
-    tmp.dir({prefix: 'mongo_'}).then((directory) => {
-      console.log('db path: ', directory.path);
-      let mongodCmd = util.format('mongod --dbpath=%s', directory.path);
-      let processCallback = function(output) {
-          console.log(output);
-          if (output.toString().indexOf('waiting for connections on port 27017')>-1) {
-            getBitfinexData();
-           }
-      };
-      nrc.run(mongodCmd, {onData: processCallback});
+    startMongoD().then(()=>{
+        getBitfinexData();
     });
   });
+  /**
+   * A function to start mongod and set the dbpath
+   * @return {Promise} promise
+   */
+function startMongoD() {
+    return new Promise((resolve, reject)=>{
+        tmp.dir({prefix: 'mongo_'}).then((directory) => {
+            console.log('db path: ', directory.path);
+            let mongodCmd = util.format('mongod --dbpath=%s', directory.path);
+            let processCallback = function(output) {
+                console.log(output);
+                if (output.toString().indexOf('waiting for connections on port 27017') > -1) {
+                    resolve();
+                }
+            };
+            let errorCallback = function(output) {
+                reject();
+            };
+            nrc.run(mongodCmd, {onData: processCallback, onError: errorCallback});
+    });
+    });
+}
+
